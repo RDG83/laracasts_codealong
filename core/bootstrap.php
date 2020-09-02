947a3d7405330e418099aaf919ca74149e15d8d2
RDG83 <rdgrijs@hotmail.com>
2020-09-02 10:44:34 +0200

    add composer and make autoloader

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,12 +4,6 @@ $app = [];
 
 $app["config"] = require "config.php";
 
-require "Request.php";
-require "Router.php";
-require "core/database/QueryBuilder.php";
-require "core/database/Connection.php";
-
-
 $app["database"] = new QueryBuilder(
     Connection::make($app["config"]["database"])
 );
